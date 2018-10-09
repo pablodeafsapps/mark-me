81f3b68c080b2954c0b47ec64115148c07888175
Pablo L <pablodeafsapps@gmail.com>
2018-10-09 21:56:27 +0200

    Finish 'main' module implementation

--- app/src/main/kotlin/com/raywenderlich/markme/main/view/ui/MainActivity.kt
+++ app/src/main/kotlin/com/raywenderlich/markme/main/view/ui/MainActivity.kt
@@ -24,12 +24,39 @@ package com.raywenderlich.markme.main.view.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.raywenderlich.markme.R
+import com.raywenderlich.markme.main.MainContract
+import com.raywenderlich.markme.main.presenter.MainPresenter
+import com.raywenderlich.markme.utils.ClassSection
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainContract.View {
+
+    private val cardAtt by lazy { activity_main__cardview__attendance }
+    private val cardGrading by lazy { activity_main__cardview__grading }
+    private val mainPresenter by lazy { MainPresenter(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cardAtt.setOnClickListener {
+            mainPresenter.onAttendanceOptionClick()
+        }
+        cardGrading.setOnClickListener {
+            mainPresenter.onGradingOptionClick()
+        }
+    }
+
+    override fun finishView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun navigateTo(section: ClassSection) {
+        when (section) {
+            ClassSection.ATTENDANCE -> Log.d("Tag", "'Attendance' clicked")
+            ClassSection.GRADING -> Log.d("Tag", "'Grading' clicked")
+        }
     }
 }
